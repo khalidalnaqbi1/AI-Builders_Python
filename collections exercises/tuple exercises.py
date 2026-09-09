9264bd0ec5d0ea035d7731ef04b6020d1f6052bf
Mariam Waleed <k.w.alnaqbi@gmail.com>
2026-09-09 21:23:22 +0400

    tuple exercises

---
+++ collections exercises/tuple exercises.py
@@ -0,0 +1,12 @@
+#tuples:orders ,unchangable ,allow duplication,Different types
+
+my_tuples= (1,2,3,4,5,5,7,8,9)
+print(my_tuples)
+print(type(my_tuples))
+
+tuple1=('kahlid', True,12,15.6)
+print(type(tuple1))
+print(tuple1)
+
+
+print(my_tuples[5:])
